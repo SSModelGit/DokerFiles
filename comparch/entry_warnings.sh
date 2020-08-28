1a6a8905d0529a4a37349822aa576752752382f6
Shashank Swaminathan <sh.swami235@gmail.com>
2020-08-27 23:15:52 -0400

    Pushing changes made to the comparch Dockerfile stuff a few days ago

--- comparch/entry_warnings.sh
+++ comparch/entry_warnings.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
+echo "Welcome to the Docker container for Computer Architecture! The container has been started successfully."
 if ! type java &> /dev/null; then
-		echo "Java is not found in this container, and may not be installed.";
-		echo "It is required to run the MARS MIPS simulator.";
+		echo "[WARNING] Java is not installed by default in the contaner.";
+		echo "[WARNING] It is not required to use the container, but it is necessary to run the MARS MIPS simulator.";
 fi
 
 exec "$@"
